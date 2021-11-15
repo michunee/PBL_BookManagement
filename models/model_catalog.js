a86b7d0a97e6d069aa172260fc51f815fc30684d
Nguyen Khoi <khoicari69gmail.com>
2021-11-15 17:18:22 +0700

    add new detail and comment, sql file

---
+++ models/model_catalog.js
@@ -0,0 +1,35 @@
+var db = require('./database'); //nhúng model database vào đế kết nối db
+var itemCat=[]; // biến để chứa dữ liệu đổ về cho controller
+var dataList=[];
+var dataListPro=[];
+
+exports.list = async () => {
+    return new Promise( (hamOK, hamLoi) => {
+        let sql = "SELECT * FROM catalog";
+        db.query(sql, (err, d) => {
+            console.log('List success');
+            dataList = d;
+            hamOK(dataList);
+        })
+        }
+    )
+}
+
+exports.listByName = async (nameCat) => {
+    
+
+    return new Promise( (hamOK, hamLoi) => {
+        let sql = `SELECT * FROM catalog WHERE nameCat='${nameCat}'`;
+        db.query(sql, (err, result) => {
+            console.log('Get idCat by nameCat success');
+            itemCat = result[0].idCat;
+        })
+        let sql2 = `SELECT * FROM product WHERE idCat=${itemCat}`;
+        db.query(sql2, (err, result) => {
+            console.log('Get list product by id Cat success');
+            dataListPro = result;
+            hamOK(dataListPro);
+        })
+        }
+    )
+}
