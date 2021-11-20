7877e011f0ce7e9e7c7a49282c47376f5016a471
anhtuyetdut <anhtuyetdutk19@gmail.com>
2021-11-20 13:10:20 +0700

    update catalog admin

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 var productRouter = require('./routes/product');
+var catalogadminRouter = require('./routes/catalog_admin');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/danh-muc', catalogRouter);
 app.use('/san-pham', productRouter);
+app.use('/book', catalogadminRouter);
 
 
 // catch 404 and forward to error handler
--- models/model_catalog.js
+++ models/model_catalog.js
@@ -1,35 +1,86 @@
 var db = require('./database'); //nhúng model database vào đế kết nối db
 var itemCat=[]; // biến để chứa dữ liệu đổ về cho controller
-var dataList=[];
+var data=[];
 var dataListPro=[];
 
 exports.list = async () => {
-    return new Promise( (hamOK, hamLoi) => {
+    return new Promise((hamOK, hamloi) =>{
         let sql = "SELECT * FROM catalog";
-        db.query(sql, (err, d) => {
-            console.log('List success');
-            dataList = d;
-            hamOK(dataList);
+        db.query(sql, (err,d) => {
+            if (err) console.log(err);
+            else
+            {
+                data = d;
+                hamOK(data);
+            } 
         })
-        }
-    )
+    })     
+}
+exports.detailCategory = async (idCat) => {
+    return new Promise((hamOK, hamloi) => {
+        let sql = "SELECT * FROM catalog WHERE idCat = " + idCat;
+        db.query(sql, (err, d) => {
+            if(err) console.log(err);
+            else
+            {
+                data = d[0];
+                hamOK(data);
+            }
+        })
+    })
+}
+exports.create = function( nameCategory, number)
+{
+    let data = {
+        nameCat: nameCategory,
+        number : number
+    };
+    let sql = 'INSERT INTO catalog SET ?';
+    let query = db.query(sql, data, (err, result) =>{
+        console.log('Create success');
+    });
+}
+
+exports.update = function(ID, NameCategory, number)
+{
+    let sql = "UPDATE bookcategory SET nameCat='" + NameCategory + "', number='" + number + "' WHERE idCat= '" + ID + "'";
+    // let sql = `UPDATE products SET 
+    // NameCategory='${NameCategory}', 
+    // number='${number}', 
+    // WHERE ID=${ID}`;
+    let query = db.query(sql,(err, result) => {
+        console.log('Update success');
+    });
+}
+
+exports.delete = function(ID)
+{
+    let sql = " DELETE FROM catalog WHERE idCat = '" + ID + "'";
+    let query = db.query(sql, (err, result) => {
+        console.log("Delete success");
+    });
 }
 
 exports.listByName = async (nameCat) => {
-    
-
     return new Promise( (hamOK, hamLoi) => {
         let sql = `SELECT * FROM catalog WHERE nameCat='${nameCat}'`;
         db.query(sql, (err, result) => {
-            console.log('Get idCat by nameCat success');
-            itemCat = result[0].idCat;
+            if(err) console.log(err);
+            else
+            {
+                console.log('Get idCat by nameCat success');
+                itemCat = result[0].idCat;
+            }
         })
         let sql2 = `SELECT * FROM product WHERE idCat=${itemCat}`;
         db.query(sql2, (err, result) => {
-            console.log('Get list product by id Cat success');
-            dataListPro = result;
-            hamOK(dataListPro);
+            if(err) console.log(err);
+            else
+            {
+                console.log('Get list product by id Cat success');
+                dataListPro = result;
+                hamOK(dataListPro);
+            }
         })
-        }
-    )
+    })
 }
